2a9cd8e9c1d734b408d337ea05b30a90bbe5081f
ArafatOmi <arafatislamomino1@gmail.com>
2024-01-09 21:59:16 +0600

    updat

--- js/script.js
+++ js/script.js
@@ -18,3 +18,30 @@ var swiper = new Swiper(".BanarSlide", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  // Define variables
+var tabLabels = document.querySelectorAll("#tabs li");
+var tabPanes = document.getElementsByClassName("tab-contents");
+
+function activateTab(e) {
+  e.preventDefault();
+  
+  // Deactivate all tabs
+  tabLabels.forEach(function(label, index){
+    label.classList.remove("active");
+  });
+  [].forEach.call(tabPanes, function(pane, index){
+    pane.classList.remove("active");
+  });
+  
+  // Activate current tab
+  e.target.parentNode.classList.add("active");
+  var clickedTab = e.target.getAttribute("href");
+  document.querySelector(clickedTab).classList.add("active");
+}
+
+// Apply event listeners
+tabLabels.forEach(function(label, index){
+  label.addEventListener("click", activateTab);
+});
